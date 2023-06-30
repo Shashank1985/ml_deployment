b86edbacebf4ad99c999dd995364f3b650225be8
sensei0410 <32988364+Shashank1985@users.noreply.github.com>
2023-06-30 19:20:50 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+#deploying the symptom to disease model using streamlit
+#done by Shashank1985 on 30/06/2023
+
+import streamlit as st
+import pandas as pd
+import pickle
+model= pickle.load(open("C:\\Users\\Prathima\\Desktop\\data science and ML\\firstd\\model.pkl",'rb'))
+def predict(text):
+    prediction = model.predict(text)
+    return prediction
+def list_to_string(s):
+    s = ""
+    for ele in s:
+        s += ele
+    return s
+st.title("""WELCOME""")
+st.header("Enter your symptoms")
+
+x = st.text_input("enter symptoms")
+x1 = [x]
+
+if st.button('Predict disease: '):
+    output = predict(x1)
+    st.success(output)
