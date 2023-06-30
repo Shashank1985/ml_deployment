3ea983d135f4e760363894f9871e37d475598747
sensei0410 <32988364+Shashank1985@users.noreply.github.com>
2023-06-30 19:32:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 import streamlit as st
 import pandas as pd
 import pickle
-model= pickle.load(open("model.pkl",'rb'))
+model= pickle.load(open("ml_deployment/model.pkl",'rb'))
 def predict(text):
     prediction = model.predict(text)
     return prediction
