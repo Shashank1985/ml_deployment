2f95cd67ecfd8b9d2f9c5e22a1212ed55dead2a4
sensei0410 <32988364+Shashank1985@users.noreply.github.com>
2024-04-25 21:48:58 +0530

    Added text details for the patient to better understand

--- main.py
+++ main.py
@@ -15,10 +15,12 @@ def list_to_string(s):
     return s
 st.title("""WELCOME""")
 st.header("Enter your symptoms")
-
+st.write("NLP models work best when you are as specific as possible with your input. Please note that AI models might not always give 100% accurate results and it is always better to consult with the doctor")
+st.write("This model helps you gain a better understanding of what disease you might have")
 x = st.text_input("enter symptoms")
 x1 = [x]
 
 if st.button('Predict disease: '):
     output = predict(x1)
-    st.success(output)
+    output = list_to_string(output)
+    st.success("The predicted disease is: " + output)
