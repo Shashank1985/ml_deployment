0c741e7eb7e1ce27c9bf4f9e8d9af6b758a94b65
sensei0410 <32988364+Shashank1985@users.noreply.github.com>
2024-04-25 21:53:21 +0530

    changed text color

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ def list_to_string(s):
     for ele in s:
         s += ele
     return s
-st.title("""WELCOME""")
+st.title("""<span style = "color:blue">WELCOME</span>""",unsafe_allow_html = True)
 st.header("Enter your symptoms")
 st.write("NLP models work best when you are as specific as possible with your input.")
 st.write("**Please note that AI models might not always give 100% accurate results and it is always better to consult with the doctor**")
 st.write("This model helps you gain a better understanding of what disease you might have")
-x = st.text_input("enter symptoms")
+x = st.text_input("<span style = "color:blue">enter symptoms</span>",unsafe_allow_html = True)
 x1 = [x]
 
 if st.button('Predict disease: '):
